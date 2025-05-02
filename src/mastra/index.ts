6f3c08980cb77335ec598b5939562b19cf609f8c
mfrachet <marvin@mastra.ai>
2025-05-02 21:11:02 +0200

    revert exporter

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -5,11 +5,11 @@ import { weatherAgent } from "./agents";
 
 import { MastraCloudExporter } from "@mastra/cloud";
 
-// Initialize the exporter with your access token
-const exporter = new MastraCloudExporter({
-  accessToken: process.env.MASTRA_CLOUD_ACCESS_TOKEN, // Your Mastra Cloud access token
-  endpoint: "https://mastra-cloud-endpoint.example.com", // Mastra cloud endpoint
-});
+// // Initialize the exporter with your access token
+// const exporter = new MastraCloudExporter({
+//   accessToken: process.env.MASTRA_CLOUD_ACCESS_TOKEN, // Your Mastra Cloud access token
+//   endpoint: "https://mastra-cloud-endpoint.example.com", // Mastra cloud endpoint
+// });
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
@@ -18,15 +18,15 @@ export const mastra = new Mastra({
     name: "Mastra",
     level: "info",
   }),
-  telemetry: {
-    serviceName: "My-Agent",
-    enabled: true,
-    sampling: {
-      type: "always_on",
-    },
-    export: {
-      type: "custom",
-      exporter,
-    },
-  },
+  // telemetry: {
+  //   serviceName: "My-Agent",
+  //   enabled: true,
+  //   sampling: {
+  //     type: "always_on",
+  //   },
+  //   export: {
+  //     type: "custom",
+  //     exporter,
+  //   },
+  // },
 });
